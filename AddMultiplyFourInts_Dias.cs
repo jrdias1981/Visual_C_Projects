f292fdf43764cb8dfd6b707e6ebd753f7287d9a0
Jonathas Dias <108290895+jrdias1981@users.noreply.github.com>
2022-09-21 21:27:12 -0400

    Add files via upload

---
+++ AddMultiplyFourInts_Dias.cs
@@ -0,0 +1,33 @@
+﻿//Software Development C#
+//Jonathas Dias
+//2-2 Module - Adding and Multiplying Four Integers
+
+using System; 
+
+namespace AddMultiplyFourInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int num1 = 13, num2 = 55, num3 = 123, num4 = 325, Sum, Product;
+
+            //Sum of given integers
+
+            Sum = num1 + num2 + num3 + num4;
+            //Multiplication of given integers
+
+            Product = num1 * num2 * num3 * num4;
+
+            //Display the output
+
+            Console.WriteLine("Jonathas Dias Copy");
+            Console.WriteLine($"The sum of {num1}, {num2}, {num3}, {num4} = {Sum}.");
+            Console.WriteLine($"The product of {num1}, {num2}, {num3}, {num4} = {Product}.");
+
+            Console.ReadKey();
+        }
+    }
+}
+
